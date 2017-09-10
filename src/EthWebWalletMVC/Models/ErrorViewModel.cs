6df4dd318512474cff0d942464ba08465b8aa31b
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2017-09-10 15:43:35 +1000

    Add files

---
+++ src/EthWebWalletMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EthWebWalletMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
