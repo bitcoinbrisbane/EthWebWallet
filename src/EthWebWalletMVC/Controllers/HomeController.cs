11e096f7f58c9639617dbf9afeea5927ae68e518
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2017-09-10 17:21:35 +1000

    Merge

--- src/EthWebWalletMVC/Controllers/HomeController.cs
+++ src/EthWebWalletMVC/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -32,42 +31,3 @@ namespace EthWebWalletMVC.Controllers
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using EthWebWalletMVC.Models;
-
-namespace EthWebWalletMVC.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
-}
->>>>>>> 6df4dd318512474cff0d942464ba08465b8aa31b
--- src/EthWebWalletMVC/Models/ErrorViewModel.cs
+++ src/EthWebWalletMVC/Models/ErrorViewModel.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 
 namespace EthWebWalletMVC.Models
@@ -9,16 +8,4 @@ namespace EthWebWalletMVC.Models
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
-=======
-using System;
-
-namespace EthWebWalletMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
->>>>>>> 6df4dd318512474cff0d942464ba08465b8aa31b
 }
